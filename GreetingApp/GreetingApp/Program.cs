086ee5730771b17df008bb2df58a5fc2470a0005
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2024-02-08 08:03:21 +0800

    February 8, 2024 (2/08/2024)

---
+++ GreetingApp/GreetingApp/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace GreetingApp
+{
+    internal class GreetingProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+
+            Console.Write("Enter the total number of your enrolled courses: ");
+            int courseNum;
+            courseNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the price of your favorite book: ");
+            double bookPrice;
+            bookPrice = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"Name: {userName}");
+            Console.WriteLine($"Total enrolled courses: {courseNum}");
+            Console.WriteLine($"Price of my favorite book: {bookPrice}");
+        }
+    }
+}
