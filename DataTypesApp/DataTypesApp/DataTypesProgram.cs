78fe7877fc48adefdbbc069814af8e10c165c592
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2024-02-16 09:19:09 +0800

    February 16, 2024 (2/16/2024)
    02 Lab Exercise 1

---
+++ DataTypesApp/DataTypesApp/DataTypesProgram.cs
@@ -0,0 +1,20 @@
+﻿namespace DataTypesApp
+{
+    class DataTypesProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the pieces of apple: ");
+            int appleCount = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write($"Enter total price of {appleCount} apple(s): ");
+            double applePrice = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"The total price of {appleCount} apple(s) is {applePrice}");
+            Console.WriteLine($"The value of original price is {applePrice}");
+
+            int applePriceInt = (int)applePrice; // will convert double to int
+            Console.WriteLine($"The value of converted price is {applePriceInt}");
+        }
+    }
+}
