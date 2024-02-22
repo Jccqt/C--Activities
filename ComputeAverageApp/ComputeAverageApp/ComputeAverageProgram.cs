6e14228b68f341f254a221f10e45b20bb62ae9fa
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2024-02-23 07:31:30 +0800

    February 23, 2024 (2/23/24)
    03 Task Performance 1

---
+++ ComputeAverageApp/ComputeAverageApp/ComputeAverageProgram.cs
@@ -0,0 +1,20 @@
+﻿namespace ComputeAverageApp
+{
+    internal class ComputeAverageProgram
+    {
+        static void Main(string[] args)
+        {
+            int studentGrade= 0;
+            decimal averageGrade;
+            Console.WriteLine("Enter 5 grades separated by new line: ");
+            for(int i = 0;i < 5;i++)
+            {
+                int gradeContainer = Convert.ToInt32(Console.ReadLine());
+                studentGrade += gradeContainer;
+            }
+            averageGrade = (decimal)studentGrade / 5;
+
+            Console.WriteLine($"The average is {averageGrade} and round off to {Math.Round(averageGrade)}");
+        }
+    }
+}
