2d37426b929ac733a497aa73ae603ed99df19155
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2024-05-31 08:56:24 +0800

    10 Hands-on Activty 1

---
+++ CashierApplication/CashierApplication/frmPurchaseDiscountItem.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace CashierApplication
+{
+    partial class frmPurchaseDiscountItem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DiscountBox = new System.Windows.Forms.TextBox();
+            this.PriceBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.QuantityBox = new System.Windows.Forms.TextBox();
+            this.ComputeBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.AmountLabel = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.PaymentBox = new System.Windows.Forms.TextBox();
+            this.SubmitBtn = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.ChangeLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Item:";
+            // 
+            // NameBox
+            // 
+            this.NameBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NameBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.NameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NameBox.Location = new System.Drawing.Point(16, 43);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(187, 24);
+            this.NameBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(216, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Discount (%):";
+            // 
+            // DiscountBox
+            // 
+            this.DiscountBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.DiscountBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.DiscountBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DiscountBox.Location = new System.Drawing.Point(220, 43);
+            this.DiscountBox.Name = "DiscountBox";
+            this.DiscountBox.Size = new System.Drawing.Size(133, 24);
+            this.DiscountBox.TabIndex = 3;
+            // 
+            // PriceBox
+            // 
+            this.PriceBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PriceBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.PriceBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PriceBox.Location = new System.Drawing.Point(66, 81);
+            this.PriceBox.Name = "PriceBox";
+            this.PriceBox.Size = new System.Drawing.Size(137, 24);
+            this.PriceBox.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Price:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(216, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Quantity:";
+            // 
+            // QuantityBox
+            // 
+            this.QuantityBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.QuantityBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.QuantityBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuantityBox.Location = new System.Drawing.Point(294, 85);
+            this.QuantityBox.Name = "QuantityBox";
+            this.QuantityBox.Size = new System.Drawing.Size(59, 24);
+            this.QuantityBox.TabIndex = 7;
+            // 
+            // ComputeBtn
+            // 
+            this.ComputeBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ComputeBtn.Location = new System.Drawing.Point(163, 122);
+            this.ComputeBtn.Name = "ComputeBtn";
+            this.ComputeBtn.Size = new System.Drawing.Size(81, 29);
+            this.ComputeBtn.TabIndex = 8;
+            this.ComputeBtn.Text = "Compute";
+            this.ComputeBtn.UseVisualStyleBackColor = true;
+            this.ComputeBtn.Click += new System.EventHandler(this.ComputeBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(62, 185);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Total amount: ";
+            // 
+            // AmountLabel
+            // 
+            this.AmountLabel.AutoSize = true;
+            this.AmountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AmountLabel.Location = new System.Drawing.Point(178, 185);
+            this.AmountLabel.Name = "AmountLabel";
+            this.AmountLabel.Size = new System.Drawing.Size(89, 20);
+            this.AmountLabel.TabIndex = 10;
+            this.AmountLabel.Text = "< amount >";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 236);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(142, 20);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Payment received: ";
+            // 
+            // PaymentBox
+            // 
+            this.PaymentBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PaymentBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.PaymentBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PaymentBox.Location = new System.Drawing.Point(151, 236);
+            this.PaymentBox.Name = "PaymentBox";
+            this.PaymentBox.Size = new System.Drawing.Size(137, 24);
+            this.PaymentBox.TabIndex = 12;
+            // 
+            // SubmitBtn
+            // 
+            this.SubmitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SubmitBtn.Location = new System.Drawing.Point(294, 233);
+            this.SubmitBtn.Name = "SubmitBtn";
+            this.SubmitBtn.Size = new System.Drawing.Size(59, 29);
+            this.SubmitBtn.TabIndex = 13;
+            this.SubmitBtn.Text = "Submit";
+            this.SubmitBtn.UseVisualStyleBackColor = true;
+            this.SubmitBtn.Click += new System.EventHandler(this.SubmitBtn_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(62, 283);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Change: ";
+            // 
+            // ChangeLabel
+            // 
+            this.ChangeLabel.AutoSize = true;
+            this.ChangeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChangeLabel.Location = new System.Drawing.Point(134, 283);
+            this.ChangeLabel.Name = "ChangeLabel";
+            this.ChangeLabel.Size = new System.Drawing.Size(125, 20);
+            this.ChangeLabel.TabIndex = 15;
+            this.ChangeLabel.Text = "< change label >";
+            // 
+            // frmPurchaseDiscountItem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 349);
+            this.Controls.Add(this.ChangeLabel);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.SubmitBtn);
+            this.Controls.Add(this.PaymentBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.AmountLabel);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ComputeBtn);
+            this.Controls.Add(this.QuantityBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.PriceBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DiscountBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.label1);
+            this.Name = "frmPurchaseDiscountItem";
+            this.Text = "Purchase Discount Item";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox QuantityBox;
+        private System.Windows.Forms.Button ComputeBtn;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label AmountLabel;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox PaymentBox;
+        private System.Windows.Forms.Button SubmitBtn;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label ChangeLabel;
+        private System.Windows.Forms.TextBox DiscountBox;
+        private System.Windows.Forms.TextBox PriceBox;
+        public System.Windows.Forms.TextBox NameBox;
+    }
+}
+
---
+++ CashierApplication/CashierApplication/frmPurchaseDiscountItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ItemNamespace;
+
+namespace CashierApplication
+{
+    public partial class frmPurchaseDiscountItem : Form
+    {
+        static DiscountItem Item;
+        public frmPurchaseDiscountItem()
+        {
+            InitializeComponent();
+        }
+
+        private void ComputeBtn_Click(object sender, EventArgs e)
+        {
+            Item = new DiscountItem(NameBox.Text, Convert.ToDouble(PriceBox.Text), Convert.ToInt32(QuantityBox.Text), Convert.ToDouble(DiscountBox.Text));
+            AmountLabel.Text = Item.getTotalPrice().ToString();
+        }
+
+        private void SubmitBtn_Click(object sender, EventArgs e)
+        {
+            if(Convert.ToDouble(AmountLabel.Text) <= Convert.ToDouble(PaymentBox.Text))
+            {
+                Item.setPayment(Convert.ToDouble(PaymentBox.Text));
+                ChangeLabel.Text = Item.getChange().ToString();
+            }
+            else
+            {
+                MessageBox.Show("Insufficient payment!");
+            }
+            
+        }
+    }
+}
