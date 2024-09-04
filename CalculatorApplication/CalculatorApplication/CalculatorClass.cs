ae3bc196e641d05cf7f27f59cbb1559066cc1431
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2024-09-04 19:50:33 +0800

    01 Lab Exercise 2 (04/09/2024)

---
+++ CalculatorApplication/CalculatorApplication/CalculatorClass.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApplication
+{
+    public delegate X Formula<X>(X arg1, X arg2);
+    internal class CalculatorClass
+    {
+        public Formula<double> formula;
+
+        public double GetSum(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public  double GetDifference(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public  double GetProduct(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public  double GetQuotient(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public event Formula<double> CalculateEvent
+        {
+            add
+            {
+                Console.WriteLine("Delegate has been added!");
+                formula += value;
+            }
+            remove
+            {
+                Console.WriteLine("Delegate has been removed!");
+                formula -= value;
+            }
+        }
+    }
+}
---
+++ CalculatorApplication/CalculatorApplication/frmCalculator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApplication
+{
+    public partial class frmCalculator : Form
+    {
+        CalculatorClass cal;
+        private double num1, num2;
+
+        private void btnEqual_Click(object sender, EventArgs e)
+        {
+            if(!txtBoxInput1.Text.Equals("") && !txtBoxInput2.Text.Equals("") && !cbOperator.Text.Equals(""))
+            {
+                // will calculate if there's no empty box/es
+
+                num1 = Convert.ToDouble(txtBoxInput1.Text.ToString());
+                num2 = Convert.ToDouble(txtBoxInput2.Text.ToString());
+
+                switch (cbOperator.SelectedItem.ToString())
+                {
+                    case "+":
+                        cal.CalculateEvent += new Formula<double>(cal.GetSum);
+                        lblDisplayTotal.Text = cal.GetSum(num1, num2).ToString();
+                        cal.CalculateEvent -= new Formula<double>(cal.GetSum);
+                        break;
+                    case "-":
+                        cal.CalculateEvent += new Formula<double>(cal.GetDifference);
+                        lblDisplayTotal.Text = cal.GetDifference(num1, num2).ToString();
+                        cal.CalculateEvent -= new Formula<double>(cal.GetDifference);
+                        break;
+                    case "*":
+                        cal.CalculateEvent += new Formula<double>(cal.GetProduct);
+                        lblDisplayTotal.Text = cal.GetProduct(num1, num2).ToString();
+                        cal.CalculateEvent -= new Formula<double>(cal.GetDifference);
+                        break;
+                    case "/":
+                        cal.CalculateEvent += new Formula<double>(cal.GetQuotient);
+                        lblDisplayTotal.Text = cal.GetQuotient(num1, num2).ToString();
+                        cal.CalculateEvent -= new Formula<double>(cal.GetQuotient);
+                        break;
+                }
+            }
+            else
+            {
+                // will prompt a message if there's an empty box/es
+                MessageBox.Show("Can't calculate if there's an empty box/es", "Empty input boxes!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // will clear the values in textboxes, combo box, and label
+            txtBoxInput1.Clear();
+            txtBoxInput2.Clear();
+            cbOperator.SelectedIndex = -1;
+            lblDisplayTotal.Text = "";
+
+        }
+
+        public frmCalculator()
+        {
+            InitializeComponent();
+            lblDisplayTotal.Text = "";
+            cal = new CalculatorClass();
+        }
+
+    }
+}
