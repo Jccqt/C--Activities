586d136b38abefe7632dd067ea1134ef5db19713
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2024-10-15 19:58:12 +0800

    04 Lab Exercise 1 (Event-Driven)

---
+++ FrmBasicThread/FrmBasicThread/FrmBasicThread.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace FrmBasicThread
+{
+    public partial class FrmBasicThread : Form
+    {
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("-Before starting thread-");
+            Thread ThreadA = new Thread(new ThreadStart(MyThreadClass.Thread1));
+            Thread ThreadB = new Thread(new ThreadStart(MyThreadClass.Thread1));
+            ThreadA.Name = "Thread A";
+            ThreadB.Name = "Thread B";
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadA.Join();
+            ThreadB.Join();
+            Console.WriteLine("-End of Thread");
+        }
+    }
+}
