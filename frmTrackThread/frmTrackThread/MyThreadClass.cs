3a719278707706f3865752cb21db4653119476c9
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2024-10-15 20:35:39 +0800

    04 Task Performance 1 (Event Driven)

---
+++ frmTrackThread/frmTrackThread/MyThreadClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace frmTrackThread
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            
+            for (int i = 0; i <= 2; i++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine($"Name of Thread: {thread.Name} = {i}");
+                Thread.Sleep(500); // will suspend the thread for 0.5 seconds
+            }
+        }
+
+        public static void Thread2()
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine($"Name of Thread: {thread.Name} = {i}");
+                Thread.Sleep(1500); // will suspend the thread for 1.5 seconds
+            }
+            
+        }
+    }
+}
---
+++ frmTrackThread/frmTrackThread/frmTrackThread.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace frmTrackThread
+{
+    public partial class frmTrackThread : Form
+    {
+        private Thread threadA, threadB, threadC, threadD;
+        public frmTrackThread()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("-Thread Starts-");
+            threadA = new Thread(new ThreadStart(MyThreadClass.Thread1));
+            threadB = new Thread(new ThreadStart(MyThreadClass.Thread2));
+            threadC = new Thread(new ThreadStart(MyThreadClass.Thread1));
+            threadD = new Thread(new ThreadStart(MyThreadClass.Thread2));
+
+            // will set the priority of the thread
+            threadA.Priority = ThreadPriority.Highest;
+            threadB.Priority = ThreadPriority.Normal;
+            threadC.Priority = ThreadPriority.AboveNormal;
+            threadD.Priority = ThreadPriority.BelowNormal;
+
+            // will set the name of the thread
+            threadA.Name = "Thread A";
+            threadB.Name = "Thread B";
+            threadC.Name = "Thread C";
+            threadD.Name = "Thread D";
+
+            threadA.Start();
+            threadC.Start();
+            threadB.Start();
+            threadD.Start();
+
+            threadA.Join();
+            threadC.Join();
+            threadB.Join();
+            threadD.Join();
+
+            Console.WriteLine("-End of Thread-");
+        }
+    }
+}
